f8217dc03ea9d61dd98bfd3356bd507eacac09e3
p538024 <p538024@SP7261ET360.corp.caixa.gov.br>
2019-07-02 19:30:22 -0300

    request and routes

--- api.php
+++ api.php
@@ -1,23 +1,61 @@
 <?php
-/*
-$request = $_SERVER['REDIRECT_URL'];
-$method = $_SERVER['REQUEST_METHOD'];
 
-switch ($request) {
-    case '/' :
-        require __DIR__ . '/views/index.php';
-        break;
-    case '' :
-        require __DIR__ . '/views/index.php';
-        break;
-    case '/about' :
-        require __DIR__ . '/views/about.php';
-        break;
-    default:
-        require __DIR__ . '/views/404.php';
-        break;
-}
- * 
- */
+include_once 'Request.php';
+include_once 'Router.php';
 
-?>
+$router = new Router(new Request);
+
+// How GET requests will be defined
+$router->get('/test-dev/api/carros', function($request)
+{
+    
+    // The $request argument of the callback 
+    // will contain information about the request
+    
+    return "Blah!";
+    
+});
+
+
+// How POST requests will be defined
+$router->post('/test-dev/api/carros', function($request)
+{
+    
+    // How to get data from request body
+    //$body = $request->getBody();
+    return json_encode($request->getBody());
+    
+});
+
+
+// How GET requests will be defined
+$router->get('/test-dev/api/carros/{id}', function($request)
+{
+    
+    // The $request argument of the callback 
+    // will contain information about the request
+    return "Bleh!";
+    
+});
+
+
+// How PUT requests will be defined
+$router->put('/test-dev/api/carros/{id}', function($request)
+{
+    
+    // The $request argument of the callback 
+    // will contain information about the request
+    return "Blih!";
+    
+});
+
+
+// How DELETE requests will be defined
+$router->delete('/test-dev/api/carros/{id}', function($request)
+{
+    
+    // The $request argument of the callback 
+    // will contain information about the request
+    return "Bloh!";
+    
+});
