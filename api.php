17e0d7a337b16245001be84dfaa82c146d4e687a
p538024 <p538024@SP7261ET360.corp.caixa.gov.br>
2019-07-02 18:21:00 -0300

    make basics

--- api.php
+++ api.php
@@ -1,4 +1,23 @@
 <?php
-//session_start();
+/*
+$request = $_SERVER['REDIRECT_URL'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '/about' :
+        require __DIR__ . '/views/about.php';
+        break;
+    default:
+        require __DIR__ . '/views/404.php';
+        break;
+}
+ * 
+ */
 
 ?>
